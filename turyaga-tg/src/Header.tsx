bae820c2e9983134848bf6d6b2ef33f7eecf5d6a
phanilevanilehyile <lololoska94@gmail.com>
2024-10-18 23:18:48 +0300

    add additional info on wallet connected

--- turyaga-tg/src/Header.tsx
+++ turyaga-tg/src/Header.tsx
@@ -1,12 +1,19 @@
-import { TonConnectButton } from '@tonconnect/ui-react';
+import { TonConnectButton, useTonAddress, useTonWallet } from '@tonconnect/ui-react';
 
 function Header() {
+  const connected = useTonAddress();
+  const wallet = useTonWallet();
+
   return (
     <>
     <header>
         <span>Подключите ваш кошелек: </span>
         <TonConnectButton className="ton-connect-button" style={{float: "right"}}/>
     </header>
+    <h1>{connected}</h1>
+    <div>
+      {wallet ? 'вы используете кошелек: ' + wallet.device.appName : 'че то не так с волетом'}
+    </div>
     </>
   );
 }
